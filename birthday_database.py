6b0416781619f5d2acda28075276eebd468198ea
Ori-i0n <orionconner00@gmail.com>
2019-10-31 21:57:24 +0530

    Add new class Database

---
+++ birthday_database.py
@@ -0,0 +1,23 @@
+# Version: 1.0-20191031
+
+import json
+
+
+class Database:
+    """Contains methods to read and write from database."""
+
+    def __init__(self):
+        self.filename = "birthdays.json"
+
+    def read(self):
+        """Reads data from database, if not found creates a new file."""
+        try:
+            with open(self.filename) as infile:
+                json.load(infile)
+        except FileNotFoundError as nofile:
+            print("File not found! Creating new one.")
+            with open(self.filename, "w") as outfile:
+                json.dump({}, outfile)
+
+
+dbs = Database().read()
