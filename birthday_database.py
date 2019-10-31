7982ffe527a1a7fc0a7dcf2268ea73b3ffd48bfb
Ori-i0n <orionconner00@gmail.com>
2019-10-31 22:43:02 +0530

    Add new class App with some new functions

--- birthday_database.py
+++ birthday_database.py
@@ -1,6 +1,7 @@
 # Version: 1.0-20191031
 
 import json
+import re
 
 
 class Database:
@@ -25,4 +26,32 @@ class Database:
             json.dump(data, outfile)
 
 
-dbs = Database().read()
+class App(Database):
+
+    def __init__(self):
+        pass
+
+
+    def parse(self, userinput):
+        pattern1 = r"(add) (\w+\s?\w*\s?\w*) (\d{4}/\d{2}/\d{2})"
+        # print(re.match(pattern1, userinput, flags=re.IGNORECASE))
+        if match:= re.match(pattern1, userinput, flags=re.IGNORECASE):
+            print("cmd:", match.group(1), "name:", match.group(2), "dob:",
+                match.group(3))
+
+    def handle_userinput(self, userinput):
+        if not userinput:
+            print("Empty UserInput!")
+        else:
+            print("Adding", userinput)
+            self.parse(userinput)
+
+
+tests = ["", "add John Doe 1999/01/01", "add Sudhanshu Mohan Joshi 1954/11/11",
+         "ADD James Hammond 1111/11/11"
+        ]
+app = App()
+
+# Testing app
+for test in tests:
+    app.handle_userinput(test)
