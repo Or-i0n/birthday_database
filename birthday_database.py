3864dcd48226151ea8d2694ff20ed3604b067176
Ori-i0n <orionconner00@gmail.com>
2019-11-01 15:16:55 +0530

    Modify method write() in class Database

--- birthday_database.py
+++ birthday_database.py
@@ -14,44 +14,91 @@ class Database:
         """Reads data from database, if not found creates a new file."""
         try:
             with open(self.filename) as infile:
-                json.load(infile)
+                return json.load(infile)
         except FileNotFoundError as nofile:
             print("File not found! Creating new one.")
             with open(self.filename, "w") as outfile:
                 json.dump({}, outfile)
+            return self.read()
+
 
     def write(self, data):
         """Writes data to database."""
+        database = self.read()
+        userid = len(database) + 1
+        newdata = {userid: data}
+        database.update(newdata)
+        print(f"{newdata=}")
+
         with open(self.filename, "w") as outfile:
-            json.dump(data, outfile)
+            json.dump(database, outfile)
+
+        print("Database:", self.read())
 
 
 class App(Database):
 
     def __init__(self):
-        pass
+        super().__init__()
 
 
     def parse(self, userinput):
-        pattern1 = r"(add) (\w+\s?\w*\s?\w*) (\d{4}/\d{2}/\d{2})"
+        pattern1 = (r"(-a) ([A-Za-z]+\s?[A-Za-z]*\s?[A-Za-z]*) "
+                    r"(\d{4}/\d{2}/\d{2})")
+        pattern2 = (r"(-y|-m|-d) (\d{2,4})"
+                    r"|([A-Za-z]+\s?[A-Za-z]*\s?[A-Za-z]*)")
+
         # print(re.match(pattern1, userinput, flags=re.IGNORECASE))
-        if match:= re.match(pattern1, userinput, flags=re.IGNORECASE):
-            print("cmd:", match.group(1), "name:", match.group(2), "dob:",
-                match.group(3))
+        if match := re.match(pattern1, userinput, flags=re.IGNORECASE):
+            # print("p1", match.groups())
+            cmd, name, dob = match.groups()
+            print(f"p1 {cmd=} {name=} {dob=}")
+            return cmd, name, dob
+        elif match := re.match(pattern2, userinput, flags=re.IGNORECASE):
+            # print("p2", match.groups())
+            if (cmd := match.group(1)) and (query := match.group(2)):
+                print(f"p2-c1 {cmd=} {query=}")
+                return cmd, query
+            elif name := match.group(3):
+                print(f"p2-c2 {name=}")
+                return name
+
+    def handle_data(self, userinput):
+        parsed_data = self.parse(userinput)
+
+        if len(parsed_data) == 3:
+            cmd, name, dob = parsed_data
+        elif len(parsed_data) == 2:
+            cmd, query = parsed_data
+        elif len(parsed_data) == 1:
+            name = parsed_data
+
+        if cmd == "-a":
+            print(f"Adding {name} {dob} in database.")
+            self.write([name, dob])
+
+
 
     def handle_userinput(self, userinput):
         if not userinput:
             print("Empty UserInput!")
         else:
-            print("Adding", userinput)
-            self.parse(userinput)
+            print("Parsing:", userinput)
+            self.handle_data(userinput)
 
 
-tests = ["", "add John Doe 1999/01/01", "add Sudhanshu Mohan Joshi 1954/11/11",
-         "ADD James Hammond 1111/11/11"
-        ]
+# tests = ["", "-a John Doe 1999/01/01", "-add Sudhanshu Mohan Joshi 1954/11/11",
+#          "ADD James Hammond 1111/11/11", "Radha Mohan", "Ram", "-year 1794",
+#          "-y 1999"
+#         ]
+# app = App()
+
+# # Testing app
+# for test in tests[:]:
+#     app.handle_userinput(test)
+#     print()
+
 app = App()
 
-# Testing app
-for test in tests:
-    app.handle_userinput(test)
+while True:
+    app.handle_userinput(input("\nEnter: "))
